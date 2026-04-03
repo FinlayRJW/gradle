3a44974ca1f10c8a85b5060826ea2500d02d73ff
Tom Tresansky <ttresansky@gradle.com>
2026-04-03 07:44:22 -0400

    Update tests to use reorganized Preconditions

--- platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/caching/CachedMissingModulesIntegrationTest.groovy
+++ platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/caching/CachedMissingModulesIntegrationTest.groovy
@@ -19,7 +19,7 @@ package org.gradle.integtests.resolve.caching
 import org.gradle.integtests.fixtures.AbstractHttpDependencyResolutionTest
 import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import org.gradle.test.precondition.Requires
-import org.gradle.test.preconditions.IntegTestPreconditions
+import org.gradle.test.preconditions.TestExecutionPreconditions
 
 class CachedMissingModulesIntegrationTest extends AbstractHttpDependencyResolutionTest {
 
@@ -545,7 +545,7 @@ Required by:
         run 'retrieve'
     }
 
-    @Requires(IntegTestPreconditions.NotParallelExecutor)
+    @Requires(TestExecutionPreconditions.NotParallelExecutor)
     @ToBeFixedForConfigurationCache(because = "task uses Configuration API")
     def "hit each remote repo only once per build and missing module"() {
         given:
